ad35680a35c9364bdf16a98dd0ba4a64bb0a8617
DEVENDRA MAURYA <dmaurya292@gmail.com>
2026-04-26 18:54:00 +0530

    Testing Void Methods

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a");
+
+            //here we are testing the LastError property of the logger object to see if it is set to "a"
+            //after calling the Log method with "a" as an argument.
+
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
