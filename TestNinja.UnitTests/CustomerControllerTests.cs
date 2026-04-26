b8b3322582768e4f2c0e92c6dcf2e8162012b123
DEVENDRA MAURYA <dmaurya292@gmail.com>
2026-04-26 18:42:38 +0530

    Core Unit Testing Techniques
    Testing the return type of method

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>()); //TypeOf is more specific than InstanceOf because it will fail if the result is a derived class of NotFound.
+            Assert.That(result, Is.InstanceOf<NotFound>()); //InstanceOf is more general than TypeOf because it will pass if the result is a derived class of NotFound.
+
+
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(1);
+            Assert.That(result, Is.TypeOf<Ok>());
+
+        }
+    }
+}
