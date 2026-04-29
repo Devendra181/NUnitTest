63fe7b7ce4c717815af44905405c7f476f6f4cf2
DEVENDRA MAURYA <dmaurya292@gmail.com>
2026-04-29 10:54:37 +0530

    External Dependencies
    Making code Loosely coupled
    
    Method injection
    
    Fake Implementation and testing

---
+++ TestNinja/Mocking/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.Mocking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+        
+            var service = new VideoService();
+            var title = service.ReadVideoTitle(new FileReader());
+
+        }
+    }
+}
